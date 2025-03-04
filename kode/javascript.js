e6bb85a45c49e1b6d6fdbb0401c0192a3b20be48
signe92 <eaa24sieb@students.eaaa.dk>
2025-03-04 12:12:08 +0100

    setup

---
+++ kode/javascript.js
@@ -0,0 +1,8 @@
+function Album(id, artist, albumName, year, genre, trackList) {
+    this.id = id;
+    this.artist = artist;
+    this.albumName = albumName;
+    this.year = year;
+    this.genre = genre;
+    this.tracklist = tracklist;
+}
