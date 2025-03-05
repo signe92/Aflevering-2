cfbb7bbadcd228b95f044103dea138c2a79c9bb3
signe92 <eaa24sieb@students.eaaa.dk>
2025-03-05 08:05:10 +0100

    2nd commit in java

--- kode/javascript.js
+++ kode/javascript.js
@@ -1,4 +1,4 @@
-// function som defineres album-objekterne. 
+// Function som defineres album-objekterne. 
 function Album(id, artist, albumName, year, genre, trackList) {
     this.id = id;
     this.artist = artist;
@@ -6,4 +6,11 @@ function Album(id, artist, albumName, year, genre, trackList) {
     this.year = year;
     this.genre = genre;
     this.tracklist = tracklist;
+}
+
+// Function der bruges til at genere HTML-strukturen
+function displayAlbumCard(album, parentid) {
+    const parentElement = document.getElementById(parentid)
+    const card = document.createElement("div");
+    card.classList.add("album-card");
 }
